04d215b3bfd55dcd94f97070511784022648a105
kawataku8 <tak.354354@gmail.com>
2021-05-10 13:41:57 +0900

    パイプ時のwait確認

--- parse/PIPE_STUDY.c
+++ parse/PIPE_STUDY.c
@@ -1,8 +1,14 @@
 #include <unistd.h>
 #include <sys/wait.h>
+#include <stdio.h>
 
 extern char **environ;
 
+typedef struct s_proc
+{
+    char *cmd[3];
+    int isend;
+}               t_proc;
 
 
 // ls | grep .c 
@@ -12,27 +18,30 @@ int main(void)
     int pipefd[2];
 
     r = pipe(pipefd);
+    char    *argv1[] = {"/bin/ls","./", NULL};
+    char    *argv2[] = {"/usr/bin/grep",".c", NULL};
+    char    *argv3[] = {"/usr/bin/wc", NULL};
 
+
+    //ls
     pid_t child1 = fork();
     if (child1 == 0) {
         close(pipefd[0]);
         dup2(pipefd[1], 1);
         close(pipefd[1]);
-        char    *argv[] = {"/bin/ls","./", NULL};
-        execve(argv[0], argv, environ);
+        execve(argv1[0], argv1, environ);
     }
 
+    //grep s
     pid_t child2 = fork();
     if (child2 == 0) {
         close(pipefd[1]);
         dup2(pipefd[0], 0);
         close(pipefd[0]);
-        char    *argv[] = {"/usr/bin/grep",".c", NULL};
-        execve(argv[0], argv, environ);
+        execve(argv2[0], argv2, environ);
     }
-
     close(pipefd[0]);
     close(pipefd[1]);
-    r = waitpid(child1, &status, 0);
+    // r = waitpid(child1, &status, 0);
     r = waitpid(child2, &status, 0);
 }
