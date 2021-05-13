f96f49a6dee7e134b12566eabdeea2374a3fd92a
kawataku8 <tak.354354@gmail.com>
2021-05-13 15:51:05 +0900

    DONE パイプ処理の仕組み

---
+++ multi_cmds/cmds.c
@@ -0,0 +1,136 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../include/cmd.h"
+#include "../libft/libft.h"
+
+extern char **environ;
+
+int ispipe(t_cmd_node *cur_cmd_node)
+{
+	if (cur_cmd_node->next != NULL)
+		return 1;
+	return 0;
+}
+
+void free_cmd_list(t_cmd_list *cmd_list)
+{
+	t_cmd_node *cur_cmd_node;
+	t_cmd_node *tmp_cmd_node;
+	int i;
+
+	cur_cmd_node = cmd_list->head;
+	while (cur_cmd_node != NULL)
+	{
+		tmp_cmd_node = cur_cmd_node->next;
+		i = 0;
+		while(cur_cmd_node->argv[i] != NULL)
+		{
+			free(cur_cmd_node->argv[i]);
+			i++;
+		}
+		free(cur_cmd_node->argv);
+		free(cur_cmd_node);
+		cur_cmd_node = tmp_cmd_node;
+	}
+
+}
+
+pid_t	start_command(char *argv[], int ispipe, int haspipe, int lastpipe[2])
+{
+	pid_t pid;
+	int newpipe[2];
+
+	if (ispipe)
+		pipe(newpipe);
+	pid = fork();
+	//子プロセス
+	if (pid == 0)
+	{
+		//左にパイプあり　入力としてパイプ[0],fd0を使う
+		if (haspipe)
+		{
+			close(lastpipe[1]);
+			dup2(lastpipe[0], 0);
+			close(lastpipe[0]);
+		}
+		//右にパイプあり　出力をpipe[1],fd1に流す
+		if (ispipe)
+		{
+			close(newpipe[0]);
+			dup2(newpipe[1], 1);
+			close(newpipe[1]);
+		}
+		// execvp(argv[0], argv);
+		execve(argv[0], argv, environ);
+	}
+	
+	// 上の子プロセスですでに入出力の受け渡しは終了しているので一個前に使ったpipeをclose
+	if (haspipe)
+	{
+		close(lastpipe[0]);
+		close(lastpipe[1]);
+	}
+	if (ispipe)
+	{
+		lastpipe[0] = newpipe[0];
+		lastpipe[1] = newpipe[1];
+	}
+	return (pid);
+}
+
+int main(void)
+{
+	int i = 0;
+	int pid, status;
+	int	haspipe = 0;
+	int	lastpipe[2] = { -1, -1 };
+
+	// char    **argv;
+	char    *argv1[] = {"/bin/ls","./", NULL};
+	char    *argv2[] = {"/usr/bin/grep",".c", NULL};
+	char    *argv3[] = {"/usr/bin/wc", NULL};
+
+
+	t_cmd_list cmds_list;
+	t_cmd_node *cmd_node;
+
+	cmd_node = (t_cmd_node*)malloc(sizeof(t_cmd_node));
+	//TODO make_cmd_node("/bin/ls ./")に置き換え
+	cmd_node->argv = ft_split("/bin/ls ./",' ');
+	// cmd_node->argv = argv1;
+	cmds_list.head = cmd_node;
+
+	cmd_node = (t_cmd_node*)malloc(sizeof(t_cmd_node));
+	cmd_node->argv = ft_split("/usr/bin/grep .c", ' ');
+	// cmd_node->argv = argv2;
+	cmds_list.head->next = cmd_node;
+
+	cmd_node = (t_cmd_node*)malloc(sizeof(t_cmd_node));
+	cmd_node->argv = ft_split("/usr/bin/wc", ' ');
+	// cmd_node->argv = argv3;
+	cmds_list.head->next->next = cmd_node;
+
+	cmds_list.head->next->next->next = NULL;
+	
+
+	t_cmd_node *cur_cmd_node = cmds_list.head;
+
+	while (cur_cmd_node != NULL)
+	{
+		cur_cmd_node->pid = start_command(cur_cmd_node->argv, ispipe(cur_cmd_node), haspipe, lastpipe);
+		if ((haspipe = ispipe(cur_cmd_node)) == 1)
+			cur_cmd_node = cur_cmd_node->next;
+		else
+			break ;
+	}
+
+	waitpid(cur_cmd_node->pid,&status,0);
+
+	free_cmd_list(&cmds_list);
+
+	exit(1);
+	return 0;
+}
---
+++ multi_cmds/main.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <../include/cmd.h>
+
+
+int main(void)
+{
+	t_cmd *cmds;
+
+	return 0;
+}
