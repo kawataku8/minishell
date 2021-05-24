da8bcb985d936144b59351594def5aff8c6a6987
kawataku8 <tak.354354@gmail.com>
2021-05-24 16:03:55 +0900

    DONE 環境変数のリスト作成

---
+++ dlist_operations/dlist_operations.c
@@ -0,0 +1,108 @@
+
+#include "../include/dlist_operations.h"
+
+//key,valueは事前にmallocされている前提
+t_env_node *make_node(char *key, char *value)
+{
+	t_env_node *new_node;
+
+	if (!(new_node = (t_env_node*)malloc(sizeof(t_env_node))))
+		return (NULL);
+	new_node->key = key;
+	new_node->value = value;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+
+	return (new_node);
+}
+
+void insert_after(t_doubly_list *stack, t_env_node *node, t_env_node *new_node)
+{
+	new_node->prev = node;
+	new_node->next = node->next;
+	if (node->next == NULL)
+		stack->tail = new_node;
+	else
+		node->next->prev = new_node;
+	node->next = new_node;
+}
+
+void insert_before(t_doubly_list *stack, t_env_node *node, t_env_node *new_node)
+{
+	new_node->prev = node->prev;
+	new_node->next = node;
+	if (node->prev == NULL)
+		stack->head = new_node;
+	else
+		node->prev->next = new_node;
+	node->prev = new_node;
+}
+
+void insert_beginning(t_doubly_list *stack, t_env_node *new_node)
+{
+	if (stack->head == NULL)
+	{
+		stack->head = new_node;
+		stack->tail = new_node;
+		new_node->prev = NULL;
+		new_node->next = NULL;
+	}
+	else
+		insert_before(stack,stack->head, new_node);
+}
+
+void insert_end(t_doubly_list *stack, t_env_node *new_node)
+{
+	if (stack->tail == NULL)
+		insert_beginning(stack, new_node);
+	else     
+		insert_after(stack, stack->tail, new_node);
+}
+
+void clear_stack(t_doubly_list *stack)
+{
+	t_env_node *cur_node;
+	t_env_node *next_env_node;
+
+	if((cur_node = stack->head) != NULL)
+	{
+		while (cur_node->next != NULL)
+		{
+			next_env_node = cur_node->next;
+			free(cur_node->key);
+			free(cur_node->value);
+			free(cur_node);
+			cur_node = next_env_node;
+		}
+		free(cur_node->key);
+		free(cur_node->value);
+		free(cur_node);
+	}
+	free(stack);
+}
+
+void remove_node(t_doubly_list *stack,t_env_node *node)
+{
+	if (node->prev == NULL)
+		stack->head = node->next;
+	else
+		node->prev->next = node->next;
+	if (node->next == NULL)
+		stack->tail = node->prev;
+	else
+		node->next->prev = node->prev;
+	free(node->key);
+	free(node->value);
+	free(node);
+}
+
+void print_doubly_list(t_doubly_list *stack)
+{
+	t_env_node *node = stack->head;
+
+	while(node)
+	{
+		printf("key:[%s],value:[%s]\n",node->key,node->value);
+		node = node->next;
+	}
+}
---
+++ env_operations/expand_env.c
@@ -0,0 +1,9 @@
+
+#include "../include/env_operations.h"
+
+t_list *expand_env(t_list *token_list)
+{
+
+
+
+}
