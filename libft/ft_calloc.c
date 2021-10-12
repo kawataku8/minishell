ad7337ead4c9d52919bcc0707a4295c00404c839
USER <user@USER-no-MacBook-Pro.local>
2021-10-12 22:35:53 +0900

    fixed norm

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkawahar <tkawahar@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: stakabay <stakabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/03 11:21:18 by tkawahar          #+#    #+#             */
-/*   Updated: 2020/07/05 13:20:26 by tkawahar         ###   ########.fr       */
+/*   Updated: 2021/10/12 22:33:40 by stakabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	void *p;
+	void	*p;
 
 	if (count == 0 || size == 0)
 	{
 		count = 1;
 		size = 1;
 	}
-	if (!(p = (void *)malloc(count * size)))
+	p = (void *)malloc(count * size);
+	if (!p)
 		return (NULL);
 	ft_bzero(p, count * size);
 	return (p);
