1c50a895b13417c893d9b6d14c8f4970d3920992
kawataku8 <tak.354354@gmail.com>
2021-10-24 20:28:03 +0900

    DONE修正:find_abscmd_pathの引数を修正

--- src/execute_comannd/exec_multi_cmds.c
+++ src/execute_comannd/exec_multi_cmds.c
@@ -6,7 +6,7 @@
 /*   By: takuya <takuya@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/14 11:08:16 by takuya            #+#    #+#             */
-/*   Updated: 2021/10/16 17:44:04 by takuya           ###   ########.fr       */
+/*   Updated: 2021/10/24 20:26:27 by takuya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void	do_multi_cmd(t_cmd_node *cmd_node, t_env_list *env_list)
 {
 	char	**dchar_envlist;
 
-	find_abscmd_path(cmd_node->argv);
+	find_abscmd_path(cmd_node->argv, env_list);
 	dchar_envlist = make_char_envlist(env_list);
 	if (execve(cmd_node->argv[0], cmd_node->argv, dchar_envlist) == -1)
 	{
